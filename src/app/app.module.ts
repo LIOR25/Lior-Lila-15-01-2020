e457027a681c2cd9296f7a1b4d5890ef2c14e498
Lior <lior2512@gmail.com>
2020-03-28 13:39:02 +0300

    removing from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,8 +27,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material';
 
 import { weatherReducer } from './store/weather.reducer';
 import { StoreModule } from '@ngrx/store';
@@ -58,8 +56,6 @@ import { from } from 'rxjs';
         MatMenuModule,
         MatToolbarModule,
         MatIconModule,
-        MatSidenavModule,
-        MatListModule,
         [
             (SweetAlert2Module.forRoot(), BrowserAnimationsModule)
         ],
